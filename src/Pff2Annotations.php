2f1efe3f96db3de73abedc7e1609ae45ebed26a6
Paolo Fagni <paolo.fagni@gmail.com>
2014-11-06 14:16:38 +0100

    firts working commit

---
+++ src/Pff2Annotations.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace pff\modules;
+
+use Minime\Annotations\Interfaces\AnnotationsBagInterface;
+use pff\Abs\AModule;
+use pff\Iface\IBeforeHook;
+use pff\Iface\IConfigurableModule;
+use Minime\Annotations\Reader;
+use Minime\Annotations\Parser;
+use Minime\Annotations\Cache\ApcCache;
+
+class Pff2Annotations extends AModule implements IBeforeHook {
+
+    /**
+     * @var Reader
+     */
+    private $reader;
+
+    /**
+     * @var AnnotationsBagInterface
+     */
+    private $classAnnotations;
+
+    /**
+     * @var AnnotationsBagInterface
+     */
+    private $methodAnnotations;
+
+    public function __construct() {
+
+        $this->reader = new Reader(new Parser(), new ApcCache());
+    }
+
+    /**
+     * Executes actions before the Controller
+     *
+     * @return mixed
+     */
+    public function doBefore() {
+        $class_name                    = get_class($this->_controller);
+        $this->classAnnotations        = $this->reader->getClassAnnotations($class_name);
+        $this->methodAnnotations       = $this->reader->getMethodAnnotations($class_name, $this->_controller->getAction());
+    }
+
+    /**
+     * @param string $name
+     * @return mixed|null
+     */
+    public function getClassAnnotation($name) {
+        return $this->classAnnotations->get($name);
+    }
+
+    /**
+     * @param string $name
+     * @return mixed|null
+     */
+    public function getMethodAnnotation($name) {
+        return $this->methodAnnotations->get($name);
+    }
+
+    public function getAllClassAnnotations(){
+        return $this->classAnnotations;
+    }
+
+    public function getAllMethodAnnotations(){
+        return $this->methodAnnotations;
+    }
+}
